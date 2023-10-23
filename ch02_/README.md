bbbdb34ccccd490492ab478c765e024ae72527d2
mox692 <moymoymox@gmail.com>
2023-10-23 22:35:37 +0900

    update

---
+++ ch02_/README.md
@@ -0,0 +1,11 @@
+### コーパスのsetup
+
+```bash
+python3 fetch_corpus_data.py
+```
+
+### co-occurence matricsの作成
+
+```bash
+python3 split_word.py
+```
--- ch02_/split_word.py
+++ ch02_/split_word.py
@@ -11,17 +11,14 @@ def file_to_str(file_name: str) -> str:
 
 # 入力文字列(結構長いやつ)を、重複のない単語の配列に分割
 # https://github.com/nltk/nltk
-def split_word(sentence: str) -> np.ndarray:
+def split_word(sentence: str):
     words = word_tokenize(sentence)
     words = [word for word in words if word.isalpha()]  # コンマやピリオドを除外
     # numpy arrayに変換
     words = np.array(words)
     # 重複を除く
-    words = np.unique(words)
-    return words
-
-# test
-# print(split_word("This is a sample sentence, with some punctuation."))
+    words_unique = np.unique(words)
+    return words, words_unique
 
 # index -> string
 def create_index_to_word_dict(words: np.ndarray) -> dict:
@@ -40,19 +37,48 @@ def create_word_to_index_dict(index_to_word_dict: dict) -> dict:
 
     return result
 
-# co-occurrence matrix の雛形を作成
-def create_co_occurrence_matrix(words: np.ndarray, word_dict: dict) -> np.ndarray:
-    len = words.size
+# co-occurrence matrix を作成
+def create_co_occurrence_matrix(words: np.ndarray, word_dict: dict, index_dict: dict) -> np.ndarray:
+    len = word_dict.__len__()
+
+    # matrixを初期化
     matrix = np.zeros((len, len))
+
+    # 行列を作成
+    for i in range(words.size - 1):
+        # i, i+1番目の2つの単語に着目
+        w1 = words[i]
+        w2 = words[i+1]
+
+        index1 = index_dict[w1]
+        index2 = index_dict[w2]
+
+        matrix[index1][index2] += 1
+        matrix[index2][index1] += 1
+
+        print(f"i: {i}, w1: {w1}, w2: {w2}, index1: {index1}, index2: {index2}, matrix[index1][index2]: {matrix[index1][index2]}, matrix[index2][index1]: {matrix[index2][index1]}\n")
+
     return matrix
 
+# co-occurence matricsを可視化
+def print_matrics(matrics: np.ndarray, file: str):
+    rows, columns = matrics.shape
+    print(f"row: {rows}, column: {columns}\n")
+
+    with open(file, 'w') as file:
+        for i in range(rows):
+            for j in range(columns):
+                file.write(f"{matrics[i][j]}, ")
+            file.write("\n")
+
+
+# 標準出力の省略をなくす
 np.set_printoptions(threshold=100000)
 input_corpus = file_to_str('/Users/s15255/work/deep-learning-from-scratch-2/ch02_/fetched_corpus_data.txt')
-words = split_word(input_corpus)
-word_dict = create_index_to_word_dict(words)
+words, words_unique = split_word(input_corpus)
+word_dict = create_index_to_word_dict(words_unique)
 index_dict = create_word_to_index_dict(word_dict)
-co_occurrence_matrix = create_co_occurrence_matrix(words, word_dict)
+co_occurrence_matrix = create_co_occurrence_matrix(words, word_dict, index_dict)
 
 
-
-print(index_dict)
+print_matrics(co_occurrence_matrix, '/Users/s15255/work/deep-learning-from-scratch-2/ch02_/matrics')
